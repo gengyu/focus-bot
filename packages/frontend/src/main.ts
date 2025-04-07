409db7b83b675dd4138406d6bcc170f75320d2bc
gengyu <476413818@qq.com>
2025-04-07 18:35:42 +0800

    修改描述

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { DraggableList } from '@drag-list/vue';
 
 const app = createApp(App);
-app.component('DraggableList', DraggableList);
+
 app.mount('#app'); 
